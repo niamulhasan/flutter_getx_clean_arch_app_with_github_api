87c78a68de227152994c0163e0c440e0b81e83d8
niamulhasan <niamulhasanbd@gmail.com>
2024-05-17 10:58:07 +0600

    github_repo_info repository implementation created

--- lib/domain/repositories/repository_info.repository.dart
+++ lib/domain/repositories/repository_info.repository.dart
@@ -1,11 +1,11 @@
-
-
-
 import 'package:dartz/dartz.dart';
 import 'package:starter_project/domain/core/entities/failure.dart';
 import 'package:starter_project/domain/core/entities/rest_response.dart';
 import 'package:starter_project/domain/entities/git_repository_info.dart';
 
 abstract interface class RepositoryInfoRepository {
-  Future<Either<Failure, RestResponse<GitRepositoryInfo>>> getTopStargazers();
+  Future<Either<Failure, RestResponse<GitRepositoryInfo>>> getTopStargazers({
+    int pageNumber = 1,
+    int perPage = 20,
+  });
 }
---
+++ lib/infrastructure/data/repositories/github_repository_info.repositoryImpl.dart
@@ -0,0 +1,28 @@
+import 'package:dartz/dartz.dart';
+import 'package:minimal_rest/minimal_rest.dart';
+import 'package:starter_project/domain/core/entities/failure.dart';
+import 'package:starter_project/domain/core/entities/rest_response.dart';
+import 'package:starter_project/domain/entities/git_repository_info.dart';
+import 'package:starter_project/domain/repositories/repository_info.repository.dart';
+
+import '../models/git_repository_info.model.dart';
+
+class GithubRepositoryInfoRepositoryImpl implements RepositoryInfoRepository {
+  @override
+  Future<Either<Failure, RestResponse<GitRepositoryInfo>>> getTopStargazers({
+    int pageNumber = 1,
+    int perPage = 20,
+  }) async {
+    Either<MinRestError, RestResponse<GitRepositoryInfo>> result =
+        await MinRest().getErrorOr(
+            "https://api.github.com/search/repositories?q=Flutter&sort=stars&order=desc&page=1&per_page=20",
+            (json) => RestResponse<GitRepositoryInfo>.fromJson(
+                  json,
+                  (json) => GitRepositoryInfoModel.fromJson(json),
+                ));
+    return result.fold(
+      (error) => Left(Failure("Failed to Fetch Data", "${error.message} (${error.code})")),
+      (response) => Right(response),
+    );
+  }
+}
